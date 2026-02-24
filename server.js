9af01bc0a9172c74da9141517b80001a24f72dfd
Cantis <148445784+Cantis10@users.noreply.github.com>
2026-02-24 14:24:02 +0000

    test

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World from Express! part 5');
+  res.json({ message: "Hello JSON from Express + Vercel!" });
 });
 
 
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.get('/test', (req, res) => {
+  res.json({ message: "Hello /test JSON!" });
 });
+
+module.exports.handler = serverless(app);
