7927fc0bec653e106cf31f2309c674e6e7a16b3a
Cantis <148445784+Cantis10@users.noreply.github.com>
2026-03-21 16:26:12 +0000

    edit

--- routes/pages/adminPages.js
+++ routes/pages/adminPages.js
@@ -3,36 +3,40 @@ const app = express.Router();
 const path = require('path');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/dashboard.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/admin/Admin_Dashboard.html'));
 });
 
 app.get('/service', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/serviceRequest.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/admin/Service_Requests.html'));
 });
 
 app.get('/contractor', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/contractor.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/admin/Contractor.html'));
+});
+
+app.get('/categories', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/admin/Edit_Categories.html'));
 });
 
 app.get('/customers', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/customers.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/admin/Customers.html'));
 });
 
 app.get('/assignment', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/assignment.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/admin/Assignment.html'));
 });
 
 
 app.get('/messages', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/messages.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/admin/Messages.html'));
 });
 
 app.get('/reports', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/reports.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/admin/Reports.html'));
 });
 
 app.get('/settings', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/admin/settings.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/admin/Settings.html'));
 });
 
 module.exports = app;
